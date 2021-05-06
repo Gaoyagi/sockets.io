2371e4eb361ada39d015d2032abdc447592edca5
Gaoyagi <aoyagigeorge@gmail.com>
2021-05-06 01:10:29 -0700

    completed sockets.io

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// landing page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+//shows that the program launched
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// event to show a user connection and disconnection
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+// logs the message the user typed
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+});
+
+// This will emit the event to all connected sockets
+io.emit('some event', { 
+    someProperty: 'some value', otherProperty: 'other value' 
+}); 
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
